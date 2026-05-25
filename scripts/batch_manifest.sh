c6b01e5a67c0823df5942f868d85155b262a4162
B286413-2025 <snumber@sms.ed.ac.uk>
2026-05-25 16:58:05 +0100

    Updated make_manifest.sh, compatible with eddie array job

---
+++ scripts/batch_manifest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+# Grid Engine options
+#$ -cwd
+#$ -t 1-13
+#$ -N manifest_generation
+# Runtime limit
+#$ -l h_rt=05:00:00
+#$ -o ../logs/$JOB_NAME.$TASK_ID.out
+#$ -e ../logs/$JOB_NAME.$TASK_ID.err
+
+set -euo pipefail
+mkdir -p ../logs
+run_dir=$(sed -n "${SGE_TASK_ID}p" filestoprocess.txt)
+./make_manifest.sh "../data_by_date/$run_dir"
+
+# To run: qsub batch_manifest.sh
--- scripts/make_manifest.sh
+++ scripts/make_manifest.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 # Inputs and outputs
 # Input dir from CLI, e.g., 05_Nov_2025
-run_dir="$1" 
+run_dir="$1"
 
 # Ouput dir from subset_metadata.sh, containing relevant activation codes
 out="${2:-$run_dir/meta_subset}"
@@ -24,6 +24,6 @@ while read -r stem; do
   # TODO: add the eddie path
   f="$run_dir/raw_data/${stem}"_R1*.fastq.gz
   r="$run_dir/raw_data/${stem}"_R2*.fastq.gz
-  
+
   printf "%s\t%s\t%s\n" "$stem" "$(realpath $f)" "$(realpath $r)" >> "$manifest"
 done < "$stems"
